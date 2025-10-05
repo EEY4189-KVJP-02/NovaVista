a9ec83c4040aca866639babdf5ecf19edfd0172d
Tharmiga12 <tharmiga23@gmail.com>
2025-10-05 19:01:51 +0530

    Implement event hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Event } from "./components/Event/Event.tsx";
 
 function App() {
   return (
     <>
+      <Event/>
     </>
   );
 }
---
+++ src/components/Event/Event.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CommonHero from "../Common/CommonHero.tsx";
+
+
+export const Event = () => {
+  return (
+    <>
+      <CommonHero
+        src={"/Images/event_hero3.jpg"}
+        title={"Event"}
+        alt={"Event"}
+        subTitle={"Make every event truly memorable."}
+      />
+     
+    </>
+  );
+};
