248075f29c308cc3b436e46d400a9c15b169c51f
Tharmiga12 <tharmiga23@gmail.com>
2025-10-28 19:03:23 +0530

    database connection setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,25 @@
-const express = require("express");
+// server.js
+import express from "express";
+import cors from "cors";
+import { checkConnection } from "./config/db.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.listen(PORT, () => {
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Start server
+app.listen(PORT, async () => {
   console.log(`Server listening on ${PORT}`);
+  try {
+    await checkConnection();
+  } catch (error) {
+    console.log("Failed to initialize the database", error);
+  }
 });
